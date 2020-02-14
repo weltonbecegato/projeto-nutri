d553db251257bccd8e512ffbf4fe2cb373ff0426
Julia Leoniani <julialeoniani@outlook.com>
2020-02-13 22:22:21 -0300

    CALCULO DO IMC PARA OS PACIENTES

--- js/main.js
+++ js/main.js
@@ -1,43 +1,55 @@
-// console.error("falha ao atualizar");
-let titulo = document.querySelector("#titulo");
-titulo.textContent = "ETECIA Nutrição e Dietética";
+let pacientes = document.querySelectorAll(".paciente");
 
-// pegar o peso
-let paciente = document.querySelector(".paciente");
-let tdPeso = paciente.querySelector(".info-peso");
-let peso = tdPeso.textContent;
+//calculo para todos os pacientes
+
+//for Atualizado (com função)
+pacientes.forEach(paciente => {
+
+    //for tradicional
+    //for (var i = 0; i < pacientes.length; i++) {
+        //   let paciente = pacientes[i];
+        
+        // pegar o peso
+        let peso = paciente.querySelector(".info-peso").textContent;
+        //pegar altura
+        let altura = paciente.querySelector(".info-altura").textContent;
+        
+        let pesoValido = true;
+        let alturaValida = true;
+        
+        
+        let tdIMC = paciente.querySelector(".info-imc");
+        // validar dados
+ 
+        
+        if (peso <= 0 || peso > 1000) {
+            pesoValido = false;
+            tdIMC.textContent = "Peso Inválido";
+            paciente.classList.add("paciente-invalido");
+        }
+        if (altura <= 0) {
+            alturaValida = false;
+            tdIMC.textContent = "altura inválida";
+            paciente.classList.add("paciente-invalido");
+        }
+
+        if (!alturaValida  && !pesoValido){
+            tdIMC.textContent = "Peso e Altura inválido";       
+        }
+        
+        
+        if (pesoValido && alturaValida) {
+            // calcular IMC
+            let imc = peso / altura **2;
+            // colocar o imc na tabela
+            tdIMC.textContent = imc.toFixed(2);
+        }
+        
+    });
+        
+        
 
 
-//pegar altura
-let tdAltura = paciente.querySelector(".info-altura");
-let altura = tdAltura.textContent;
-
-let pesoValido = true;
-let alturaValida = true;
 
 
 
-// validar dados
-if (peso <= 0 || peso > 1000){
-    console.error("peso inválido");
-    pesoValido = false;
-}
-if (altura <= 0 ){
-    console.error("altura inválida");
-    alturaValida = false;
-}
-
-
-if (pesoValido  && alturaValida ) {
-
-// calcular IMC
-let imc = peso / (altura * altura);
-//console.log (imc);
-
-// para exponenciação pode colocar let imc = peso / altura ** 2;
-
-// colocar o imc na tabela
-
-let tdIMC = paciente.querySelector(".info-imc");
-tdIMC.textContent = imc;
-}
